98f6f286194844c8143388b2d2ba3cd53de56bd7
genobre <geovannasnobre@gmail.com>
2019-07-11 19:42:17 -0300

    upgrading login system

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,36 @@
-from flask import Flask, request, render_template, url_for, redirect
+# -*- coding: utf-8 -*-
 
+from flask import Flask, request, render_template, url_for, redirect, session
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 app = Flask(__name__)
+
+app.secret_key = 'gfU7Lj7r3E'
+
 app.config['MYSQL_HOST'] = 'sql10.freemysqlhosting.net'
 app.config['MYSQL_USER'] = 'sql10295050'
 app.config['MYSQL_PASSWORD'] = 'lBShTY5uYZ'
 app.config['MYSQL_DB'] = 'sql10295050'
+
 mysql = MySQL(app)
 
-@app.route("/index")
+@app.route("/index", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", msg='')
 
 @app.route("/cadastrar")
 def cadastrar():
     return render_template("cadastro.html")
 
-@app.route("/cadastro", methods=['GET','POST'])
+@app.route("/cadastro", methods=['POST'])
 def cadastro():
     if request.method == "POST":
         cur = mysql.connection.cursor()
-        name = request.form.get("Nome")
-        email = request.form.get("Email")
-        password = request.form.get("Senha")
+        name = str(request.form["Nome"])
+        email = str(request.form["Email"])
+        password = str(request.form["Senha"])
 
         if name and email:
             cur.execute('''INSERT INTO users (name, email, password) VALUES (%s, %s, %s)''', (name, email, password))
@@ -31,32 +38,39 @@ def cadastro():
 
     return redirect(url_for('index'))
 
-@app.route("/perfil")
+@app.route("/perfil",  methods=['GET', 'POST'])
 def perfil():
-    if request.method == "GET":
-        cur = mysql.connection.cursor()
-        email = request.values.get("Email")
-        senha = request.values.get("Senha")
+    msg = ''
 
-        cur.execute('''SELECT id FROM users WHERE (email = '%s')''',(email))
-        id = cur.fetchone()
+    if request.method == 'POST' and 'Email' in request.form and 'Senha' in request.form and 'TipoUsuario' in request.form:
+        
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        
+        email = str(request.form["Email"])
+        senha = str(request.form["Senha"])
+
+        cur.execute('''SELECT * FROM users WHERE email = %s AND password = %s''',(email, senha))
+        account = cur.fetchone()
     
-    if(id != None):
-        cur.execute('''SELECT name FROM users WHERE (id = %s)''',[id])
-        name = cur.fetchone()
-        cur.execute('''SELECT doctor_id FROM users WHERE (id = %s)''',[id])
-        doctor_id = cur.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['name'] = account['name']
 
-        if (doctor_id != None):
-            cur.execute('''SELECT name FROM doctors WHERE (id = %s)''',[doctor_id])
-            doctor_name = cur.fetchone()
-        else: 
-            doctor_name = None
+            cur.execute('''SELECT doctor_id FROM users WHERE email = %s''',[email])
+            doctor_id = cur.fetchone()
+            
+            if (doctor_id != None):
+                cur.execute('''SELECT name FROM doctors WHERE (id = %s)''',[doctor_id])
+                doctor_name = cur.fetchone()
+            else:
+                doctor_name = None
 
-        return render_template("calendario.html",id=id, name=name, doctor_id=doctor_id,doctor_name=doctor_name)
-    
+            return render_template("calendario.html", doctor_id=doctor_id,doctor_name=doctor_name)
+        else:
+            return render_template("index_erro.html", msg = 'Senha incorreta.')
     else:
-        return redirect(url_for('index'))
+        return render_template("index_erro.html", msg = 'Favor preencher todos os campos.')
 
 if __name__ == '__main__':
     app.run(debug=True,port=8085)

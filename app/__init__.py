655624eb1ada4291f31beb2cb23ed27071e01c84
genobre <geovannasnobre@gmail.com>
2019-07-12 00:38:58 -0300

    a

--- app/__init__.py
+++ app/__init__.py
@@ -48,8 +48,6 @@ def perfil():
         
         email = str(request.form["Email"])
         senha = str(request.form["Senha"])
-<<<<<<< HEAD
-<<<<<<< HEAD
         tipo = request.form["TipoUsuario"]
 
         if tipo == '1':
@@ -67,8 +65,10 @@ def perfil():
                 if (doctor_id != None):
                     cur.execute('''SELECT name FROM doctors WHERE (id = %s)''',[doctor_id])
                     doctor_name = cur.fetchone()
+                    session['has_doctor'] = True
                 else:
                     doctor_name = None
+                    session['has_doctor'] = False
 
                 return render_template("usertouser.html")
             else:
@@ -88,57 +88,6 @@ def perfil():
                 return render_template("index_erro.html", msg = 'Senha incorreta.')
     else:
         return render_template("index_erro.html", msg = 'Favor preencher todos os campos.')
-
-@app.route('/logout')
-def logout():
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('name', None)
-   session.pop('crm', None)
-   
-   return redirect(url_for('index'))
-=======
-=======
-        tipo = request.form["TipoUsuario"]
->>>>>>> 444fcebda501f8dead13715fdb16cd08fd57ad41
-
-        if tipo == '1':
-            cur.execute('''SELECT * FROM users WHERE email = %s AND password = %s''',(email, senha))
-            account = cur.fetchone()
-        
-            if account:
-                session['loggedin'] = True
-                session['id'] = account['id']
-                session['name'] = account['name']
-
-                cur.execute('''SELECT doctor_id FROM users WHERE email = %s''',[email])
-                doctor_id = cur.fetchone()
-                
-                if (doctor_id != None):
-                    cur.execute('''SELECT name FROM doctors WHERE (id = %s)''',[doctor_id])
-                    doctor_name = cur.fetchone()
-                else:
-                    doctor_name = None
-
-                return render_template("usertouser.html")
-            else:
-                return render_template("index_erro.html", msg = 'Senha incorreta.')
-        else:
-            cur.execute('''SELECT * FROM doctors WHERE email = %s AND password = %s''',(email, senha))
-            account = cur.fetchone()
-        
-            if account:
-                session['loggedin'] = True
-                session['id'] = account['id']
-                session['name'] = account['name']
-                session['crm'] = account['crm']
-
-                return render_template("doctortodoctor.html")
-            else:
-                return render_template("index_erro.html", msg = 'Senha incorreta.')
-    else:
-        return render_template("index_erro.html", msg = 'Favor preencher todos os campos.')
->>>>>>> 75670cab031053a065ac00026d59d876a42f7665
 
 @app.route('/logout')
 def logout():
